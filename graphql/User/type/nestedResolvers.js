51317b8ec7fcc4b4b9e4c9a8eff241123c15e607
Samir <samir@nevereatalone.io>
2020-03-25 19:58:52 +0100

    remove obselete code

--- graphql/User/type/nestedResolvers.js
+++ graphql/User/type/nestedResolvers.js
@@ -1,6 +1,4 @@
 const Skill = require('../../../model/Skill');
-const Availability = require('../../../model/Availability');;
-const {performCalendarSync} = require('../../../utils/google')
 
 module.exports.nestedUserSkillsResolver = async function (user) {
     try{
@@ -12,9 +10,6 @@ module.exports.nestedUserSkillsResolver = async function (user) {
 };
 module.exports.nestedAvailabilityResolver = async function (user, {timeFrameInHours=120}) {
     try{
-        //Perform a calendar sync if needed
-        await performCalendarSync(user, timeFrameInHours);
-
         return "To do";
     }catch (error) {
         console.debug(error);
