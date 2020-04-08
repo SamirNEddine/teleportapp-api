454fcddc824873b9d8c1f54f39a5e912d3ec8a51
Samir <samir@nevereatalone.io>
2020-04-08 16:37:11 +0200

    add AvailabilityProfile model

---
+++ model/AvailabilityProfile.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AvailabilityProfile = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    minAvailableSlotInMinutes: {
+        type: Number,
+        required: true
+    },
+    minFocusSlotInMinutes: {
+        type: Number,
+        required: true
+    },
+});
+
+module.exports = new mongoose.model('availability profile', AvailabilityProfile);
