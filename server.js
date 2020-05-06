476d4ba07fcc94b6462e1379b40ff59c34fcf19d
Samir <samir@nevereatalone.io>
2020-05-06 16:58:56 +0200

    remove test function

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {httpTimezoneCheck} = require('./middleware/timezone');
 const {integrationsCheck} = require('./middleware/integrations');
 const bodyParser = require('body-parser');
 const appUpdateServer = require('./utils/nuts');
-myUndefinedFunction();
+
 /** Connect to the database **/
 connectToDb();
 
